0ca0b8f6539cf8e9fc9c386286ca4e616a8de083
Vishwas-gour <vishwasgour2002@gmail.com>
2026-01-12 22:06:31 +0530

    add health cheacker

---
+++ src/main/java/com/web/room/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.web.room.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<?> health() {
+        return ResponseEntity.ok ("OK");
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<?> healthCheck() {
+        return ResponseEntity.ok ("OK");
+    }
+}
