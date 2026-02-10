e9a7f0f46a97d1a793bd87f91dfe56eab2f899fd
Vishwas-gour <vishwasgour2002@gmail.com>
2026-02-10 21:35:52 +0530

    allow all origins

--- src/main/java/com/web/room/controller/WishlistController.java
+++ src/main/java/com/web/room/controller/WishlistController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/wishlist")
-@CrossOrigin(origins = "http://localhost:5173")
 public class WishlistController {
 
     private final WishlistService wishlistService;
