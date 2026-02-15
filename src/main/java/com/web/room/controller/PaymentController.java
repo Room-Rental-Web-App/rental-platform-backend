6e1f9dd47d8251f4645c6f4967874b3c134dce04
Vishwas-gour <vishwasgour2002@gmail.com>
2026-02-15 10:04:13 +0530

    temporary changes

--- src/main/java/com/web/room/controller/PaymentController.java
+++ src/main/java/com/web/room/controller/PaymentController.java
@@ -1,23 +1,14 @@
 package com.web.room.controller;
 
 
-import com.razorpay.Order;
-import com.razorpay.RazorpayClient;
-import com.razorpay.RazorpayException;
-import com.razorpay.Utils;
 import com.web.room.service.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.Map;
 
 
